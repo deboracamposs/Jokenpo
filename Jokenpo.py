661e927204d22a0608e47ea4ae4bdf49c35cd9a6
Débora Campos <127258657+deboracamposs@users.noreply.github.com>
2023-10-05 22:51:51 -0300

    Update Jokenpo.py

--- Jokenpo.py
+++ Jokenpo.py
@@ -1,5 +1,4 @@
 print(' º▬▬▬▬º BEM VINDO AO JOKENPO º▬▬▬▬º')
-print()
 print('Primeira regra: Não há regras! \n'
       'Digite seus nomes ↓')
 jogador_1=input('Seu nome:' ).upper()
@@ -7,12 +6,12 @@ jogador_2=input('Seu nome:'  ).upper()
 print(f'Bem vindo {jogador_1} e {jogador_2}! Preparados para a disputa?')
 print('QUE OS JOGOS COMECEM!')
 while True:
-    opcaodoj1 = input(f'{jogador_1}, escolha entre → Pedra -- Papel -- Tesoura, para começar o disputa: ')
-    opcaodoj2 = input(f'{jogador_2}, escolha entre → Pedra -- Papel -- Tesoura, para começar o disputa: ')
+    opcaodoj1 = input(f'{jogador_1}, escolha entre → Pedra - Papel - Tesoura, para começar o disputa: ')
+    opcaodoj2 = input(f'{jogador_2}, escolha entre → Pedra - Papel - Tesoura, para começar o disputa: ')
     while opcaodoj1 == opcaodoj2:
         print(f' {opcaodoj1} bate com {opcaodoj2} → TEMOS UM EMPATE! ¯\_(ツ)_/¯ NOVA DISPUTA!! ')
-        opcaodoj1 = input(f'{jogador_1}, escolha entre → Pedra -- Papel -- Tesoura, para começar o disputa: ')
-        opcaodoj2 = input(f'{jogador_2}, escolha entre → Pedra -- Papel -- Tesoura, para começar o disputa: ')
+        opcaodoj1 = input(f'{jogador_1}, escolha entre → Pedra - Papel - Tesoura, para começar o disputa: ')
+        opcaodoj2 = input(f'{jogador_2}, escolha entre → Pedra - Papel - Tesoura, para começar o disputa: ')
     if opcaodoj1 == 'pedra' and opcaodoj2 == 'tesoura':
         print(f'A {opcaodoj1} quebra a {opcaodoj2} (×﹏×) . {jogador_1} VENCEU! ( ͡ᵔ ͜ʖ ͡ᵔ) ')
     elif opcaodoj1 == 'pedra' and opcaodoj2 == 'papel':
